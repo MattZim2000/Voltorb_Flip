5b9110b889013a497a9a8a6c595f5e2872274ab1
Carson <140027851+Stealthy-Aka-Carson@users.noreply.github.com>
2023-07-24 19:11:20 -0500

    Added work done before Github.

--- app/src/main/java/com/example/voltorbflip/MainActivity.kt
+++ app/src/main/java/com/example/voltorbflip/MainActivity.kt
@@ -16,6 +16,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
+
+        // Temporary High Score Idea: An array holding scores for the current game session.
+
+        // Other Potential Idea: View Models & Live Data?
+
+        // TODO: Create an empty array of 25 length.
+
+        // TODO: Create an Integer var to hold the ever changing score.
     }
+
+    // TODO: Create the startGame method for restarts and to begin the game.
+    // fun startGame(Array) {
+    // Randomly select 0 (Voltorb Bomb), 1, 2, or 3 for each element.
+// }
+
+    // TODO: Create the flipTile method for onClickListeners to simply the code.
+    // fun flipTile(Integer from Array) {}
 }
 
